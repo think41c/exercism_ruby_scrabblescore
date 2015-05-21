16125422f38d54e8c5b479569de58d5c23a716db
Tony DiNitto <think41c@gmail.com>
2015-05-21 02:16:26 -0400

    Basic logic for letter score set.

--- scrabble_score.rb
+++ scrabble_score.rb
@@ -1,18 +1,20 @@
 class Scrabble 
   
   def initialize(letter)
-    letter_values = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, 
+    @letter_values = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, 
                       r: 1, s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, 
                       m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4,
                       k: 5, j: 8, x: 8, q: 10, z: 10
                     }
-
+    @letter = letter
   end
 
   def score
-    0
+    p @letter_values[:t]
+    p @letter
+    p @letter_values[:@letter]
   end
 end
 
-a = Scrabble.new('t')
-puts a
+a = Scrabble.new('t').score
+a
